c934c9386824c022edcbf420641282a5c0d2c1a3
LeeHyungJoon <potter1765@nate.com>
2015-03-02 17:42:56 +0900

    sql파일추가

---
+++ GradeCal/grade.sql
@@ -0,0 +1,7 @@
+create table grade(
+name  char(20) not null,
+sub   int(11),
+grade char(4),
+point float,
+primary key(name)
+);
--- lib/dbconn.php
+++ lib/dbconn.php
@@ -3,4 +3,10 @@
         die( "SQL server에 연결할 수 없습니다."); 
 
     mysql_select_db("test",$connect);
+
+
+   // $connect=mysql_connect( "localhost", "hj", "gudwns2") or  
+   //     die( "SQL server에 연결할 수 없습니다."); 
+
+ //   mysql_select_db("hj_db",$connect);
 ?>
