421b4067ccbfbf534c3731c0ab1c1f321c291cbe
임병준 <imbyungjun@MacBook-Air.local>
2015-03-07 23:31:17 +0900

    recover

--- lib/top_menu1.php
+++ lib/top_menu1.php
@@ -2,4 +2,5 @@
 <div class="menus"><a href="./free/list.php"><img src="./img/menu2.gif" border="0"></a></div>
 <div class="menus"><input type="image"  src="img/menu3.gif" onclick="window.open('GradeCal/GradeCal.php?user=<?=$_SESSION[userid]?>', '','scrollbars=no, toolbars=no,width=450,height=800')" border="0"></div>
 <div class="menus"><a href="./sitemap/sitemap.php"><img src="./img/menu4.gif" border="0"></a></div>
-<div class="menus"><img src="./img/menu01.gif" onclick="window.open('./survey/survey.php', '','scrollbars=no, toolbars=no,width=180,height=230')" border="0"></div>
+<div class="menus"><a href="./chat/chat.php"><img src="./img/menu5.gif" border="0"></a></div>
+<div class="menus"><img src="./img/menu01.gif"></div>
